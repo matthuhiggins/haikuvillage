3ea14fe38446d59590a18bc78fbe20ab7b671cba
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2006-12-24 03:05:03 +0000

    haikus are for you and me.
    git-svn-id: svn://spiz.us/haiku/trunk@80 14663b11-d54d-c041-aa2c-fbbaee43d5f3

---
+++ app/controllers/my_haiku_controller.rb
@@ -0,0 +1,37 @@
+class MyHaikuController < ApplicationController
+  layout "haikus"
+
+  before_filter :authorize
+  
+  def add_haiku_to_favorites
+    @haiku = Haiku.find(params[:id])
+    @haiku.haiku_favorites.create(:user_id => session[:user_id])
+  end
+  
+  def remove_haiku_from_favorites
+    HaikuFavorite.delete_all("user_id = #{session[:user_id]} and haiku_id = #{params[:id]}")
+    @haiku = Product.update(params[:id],  "haiku_favorites_count = haiku_favorites_count - 1")
+  end
+  
+  def add_tags_to_haiku
+    tags = params[:tags]
+    @haiku = Haiku.find(params[:id])
+    tags.split.each do |tag|
+      Tag.add_haiku_tag(tag, @haiku)
+    end
+  end
+  
+  def add_comment
+    @haiku = Haiku.find(params[:haiku][:id])
+    comment = @haiku.comments.new(params[:comment])
+    comment.haiku = @haiku
+    comment.user_id = session[:user_id]
+    comment.save
+    redirect_to :back
+  end
+  
+  def delete_comment
+    HaikuComment.delete(params[:id])
+    redirect_to :back
+  end
+end
