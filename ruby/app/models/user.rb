31d37698680eeda4cdcf4a862498a5fbea9a77d3
mike <mike@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2007-02-13 05:56:48 +0000

    more school to group
    git-svn-id: svn://spiz.us/haiku/trunk@116 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/models/user.rb
+++ ruby/app/models/user.rb
@@ -4,8 +4,8 @@ class User < ActiveRecord::Base
   has_many :favorites, :through => :haiku_favorites, :source => :haiku
   has_many :haiku_favorites
   has_many :haikus
-  has_many :school_users
-  has_many :schools, :through => :school_users
+  has_many :group_users
+  has_many :groups, :through => :group_users
   
   attr_accessor :password_confirmation
 
