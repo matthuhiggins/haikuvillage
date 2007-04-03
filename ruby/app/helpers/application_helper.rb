c79a69a48cd9d7f55903ed5cc631ef49324f0eeb
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2007-04-03 05:43:37 +0000

    Yeeha!
    git-svn-id: svn://spiz.us/haiku/trunk@174 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/helpers/application_helper.rb
+++ ruby/app/helpers/application_helper.rb
@@ -14,21 +14,28 @@ module ApplicationHelper
     end
   end
   
-  def yui_asset_tag(source)
-    source = compute_public_path("#{source}", "yui/#{source}/assets", 'css')
-    tag("link", { "rel" => "Stylesheet", "type" => "text/css", "media" => "screen", "href" => source })
-  end
-  
-  def yui_javascript_tag(source)
-    source = compute_public_path("#{source}-min", "yui/#{source}", 'js')
-    content_tag("script", "", { "type" => "text/javascript", "src" => source })
+  def yui_component_tag(source)
+    if ["yahoo-dom-event"].include?(source)
+      file_name = "#{source}"
+    elsif ["button", "element"].include?(source)
+      file_name = "#{source}-beta-min"
+    else
+      file_name = "#{source}-min"
+    end
+    
+    if ["reset", "fonts"].include?(source)
+      stylesheet_link_tag("/yui/#{source}/#{source}")
+    else    
+      javascript_include_tag("/yui/#{source}/#{file_name}")
+    end
   end
   
   def yui_include_tag(*sources)
     tags = []
     sources.each do |source|
-      tags << yui_asset_tag(source) if defined?(RAILS_ROOT) && File.exists?("#{RAILS_ROOT}/public/yui/#{source}/assets/#{source}.css")
-      tags << yui_javascript_tag(source)
+      tags << stylesheet_link_tag("/yui/#{source}/assets/#{source}") if defined?(RAILS_ROOT) && 
+          File.exists?("#{RAILS_ROOT}/public/yui/#{source}/assets/#{source}.css")
+      tags << yui_component_tag(source)
     end
     tags.join("\n")
   end
