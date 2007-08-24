8230afdce77d30a5799f2a0321dcd71c87c88876
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2007-08-24 02:08:58 +0000

    Refactoring.
    git-svn-id: svn://spiz.us/haiku/trunk@201 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/search_controller.rb
+++ ruby/app/controllers/search_controller.rb
@@ -3,21 +3,20 @@ class SearchController < ApplicationController
   layout "haikus"
 
   def haikus
-    unless params[:q].blank?
-      current = 1 unless params[:p]
-      @haikus = Haiku.paginating_ferret_search({:q => params[:q],
-                                                :current => 1,
-                                                :page_size => 4})
-    end
+    @haikus = load_items(Haiku)
   end
   
   def groups
-    unless params[:q].blank?
-      current = 1 unless params[:p]
-      @groups = Group.paginating_ferret_search({:q => params[:q],
-                                                :current => 1,
-                                                :page_size => 4})
-    end
+    @groups = load_items(Group)
   end
   
+  private
+    def load_items(klass)
+      unless params[:q].blank?
+        current = 1 unless params[:p]
+        klass.paginating_ferret_search({:q => params[:q],
+                                        :current => 1,
+                                        :page_size => 4})
+      end
+    end
 end
