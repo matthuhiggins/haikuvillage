178932994aadadab1e3250a2a347dffc2a178766
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2007-08-27 06:20:48 +0000

    git-svn-id: svn://spiz.us/haiku/trunk@245 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/vendor/plugins/yeeha/lib/yeeha.rb
+++ ruby/vendor/plugins/yeeha/lib/yeeha.rb
@@ -1,7 +1,14 @@
+require 'set'
 module Higgode
   module Yeeha #:nodoc:
+    def build_requirements(libraries)
+      libraries.inject(SortedSet.new(libraries)) do |requirements, library|
+        requirements.merge(@@dependencies[library])
+      end.to_a
+    end
+    
     def yui_component_tag(source)
-      if ["yahoo-dom-event"].include?(source)
+      if ["yahoo-dom-event", "utilities"].include?(source)
         file_name = "#{source}"
       elsif ["button", "element"].include?(source)
         file_name = "#{source}-beta-min"
@@ -10,14 +17,14 @@ module Higgode
       end
   
       if ["reset", "fonts"].include?(source)
-        stylesheet_link_tag("/yui/#{source}/#{source}")
+        stylesheet_link_tag("http://yui.yahooapis.com/2.3.0/build/#{source}/#{source}")
       else    
-        javascript_include_tag("/yui/#{source}/#{file_name}")
+        javascript_include_tag("http://yui.yahooapis.com/2.3.0/build/#{source}/#{file_name}")
       end
     end
   
     def yui_include_tag(*sources)
-      tags = []
+      tags = [yui_component_tag('utilities')]
       sources.each do |source|
         tags << stylesheet_link_tag("/yui/#{source}/assets/#{source}") if defined?(RAILS_ROOT) && 
             File.exists?("#{RAILS_ROOT}/public/yui/#{source}/assets/#{source}.css")
