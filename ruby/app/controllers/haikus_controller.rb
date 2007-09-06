ba97f7b4b274fcf6230641cb32f70b4d2571b9a7
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2007-09-06 03:07:12 +0000

    Invalid
    git-svn-id: svn://spiz.us/haiku/trunk@284 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/haikus_controller.rb
+++ ruby/app/controllers/haikus_controller.rb
@@ -1,22 +1,22 @@
 class HaikusController < ApplicationController
   def index
-    render_index
+    render_paginated
   end
   
   def popular
-    render_index{ paginated_haikus(:order => "haiku_favorites_count desc") }
+    render_paginated{ paginated_haikus(:order => "haiku_favorites_count desc") }
   end
   
   def recent
-    render_index{ paginated_haikus(:order => "created_at desc") }
+    render_paginated{ paginated_haikus(:order => "created_at desc") }
   end
   
   def search
     unless params[:q].blank?
-      current = 1 unless params[:p]
-      @haikus = Haiku.paginating_ferret_search({:q => params[:q],
-                                        :current => 1,
-                                        :page_size => 4})
+      render_paginated(:search) do
+        current = 1 unless params[:p]
+        Haiku.paginating_ferret_search({:q => params[:q], :current => 1, :page_size => 4})
+      end
     end
   end
   
@@ -27,12 +27,12 @@ class HaikusController < ApplicationController
     ]
   end
   
-  def render_index
+  def render_paginated(action = :index)
     @haikus = block_given? ? yield : paginated_haikus
     if params[:page]
       render :partial => 'shared/haikus_paginated', :locals => { :haikus => @haikus }
     else
-      render :action => :index
+      render :action => action
     end    
   end
 end
