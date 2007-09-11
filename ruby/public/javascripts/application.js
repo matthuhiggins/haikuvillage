fbe044d8a6b19fa1c4b86cbe68cae25686e8b7f5
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2007-09-11 07:01:09 +0000

    Namespaced the javascript
    git-svn-id: svn://spiz.us/haiku/trunk@300 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/public/javascripts/application.js
+++ ruby/public/javascripts/application.js
@@ -33,53 +33,56 @@ function getElementInText(text, div_id) {
 	return newElements.length > 0 ? newElements[0] : null;
 }
 
-function paginate(request, isNext) {
+Village = {};
+Village.util = {
+	paginate: function (request, isNext) {
 	var start = isNext ? 0 : '-600px';
-	var attributes = { 'margin-left': { to: -300 } };
+		var attributes = { 'margin-left': { to: -300 } };
 
-    var newFragment = getElementInText(request.responseText, "haiku_center");
-    var newPagination = getElementInText(request.responseText, "pagination");
+    	var newFragment = getElementInText(request.responseText, "haiku_center");
+    	var newPagination = getElementInText(request.responseText, "pagination");
 
-	original = getEl("haiku_center").innerHTML;
-	getEl("haiku_left").innerHTML = original;
-	getEl("haiku_right").innerHTML = original;
-    YAHOO.util.Dom.setStyle("haiku_hidden", 'margin-left', start);
+		original = getEl("haiku_center").innerHTML;
+		getEl("haiku_left").innerHTML = original;
+		getEl("haiku_right").innerHTML = original;
+    	YAHOO.util.Dom.setStyle("haiku_hidden", 'margin-left', start);
 
-    getEl("haiku_center").innerHTML = newFragment.innerHTML;
-    getEl("pagination").innerHTML = newPagination.innerHTML;
-    var anim = new YAHOO.util.Anim('haiku_hidden', attributes, 0.6, YAHOO.util.Easing.easeOut);
-    anim.animate();
-}
+    	getEl("haiku_center").innerHTML = newFragment.innerHTML;
+    	getEl("pagination").innerHTML = newPagination.innerHTML;
+    	var anim = new YAHOO.util.Anim('haiku_hidden', attributes, 0.6, YAHOO.util.Easing.easeOut);
+    	anim.animate();
+	},
 
-function addHaiku(text) {
-    var haikuList = getEl("haiku_box");
+	addHaiku: function (text) {
+    	var haikuList = getEl("haiku_box");
 	
-    var fadeIn = function () {
-		var newHaikuDiv = createDomFromText(text);
+    	var fadeIn = function () {
+			var newHaikuDiv = createDomFromText(text);
 				
-		YAHOO.util.Dom.setStyle(newHaikuDiv, 'color', '#fff');
-		haikuList.replaceChild(newHaikuDiv, emptyHaikuDiv);
-		var colorAttributes = {
-			color: { from: '#fff', to:'#000' }
-		};
+			YAHOO.util.Dom.setStyle(newHaikuDiv, 'color', '#fff');
+			haikuList.replaceChild(newHaikuDiv, emptyHaikuDiv);
+			var colorAttributes = {
+				color: { from: '#fff', to:'#000' }
+			};
 		
-		var colorAnim = new YAHOO.util.ColorAnim(newHaikuDiv.id, colorAttributes, 0.5, YAHOO.util.Easing.easeOut);
-		colorAnim.animate();		
-	    if (YAHOO.util.Dom.getChildren(haikuList).length > 4) {
-            haikuList.removeChild(YAHOO.util.Dom.getLastChild(haikuList));
+			var colorAnim = new YAHOO.util.ColorAnim(newHaikuDiv.id, colorAttributes, 0.5, YAHOO.util.Easing.easeOut);
+			colorAnim.animate();		
+	    	if (YAHOO.util.Dom.getChildren(haikuList).length > 4) {
+            	haikuList.removeChild(YAHOO.util.Dom.getLastChild(haikuList));
+			}
+		}
+		
+		var emptyHaikuDiv = createDomFromText("<div class='haiku' id='empty_haiku'>&nbsp;</div>");
+		if (YAHOO.util.Dom.getChildren(haikuList).length === 0) {
+			haikuList.appendChild(emptyHaikuDiv);
+			emptyHaikuDiv = getEl("empty_haiku");
+			fadeIn();
+    	} else {	   
+	    	YAHOO.util.Dom.setStyle(emptyHaikuDiv, 'margin-top', '-90px');	
+	    	emptyHaikuDiv = YAHOO.util.Dom.insertBefore(emptyHaikuDiv, YAHOO.util.Dom.getFirstChild(haikuList));
+        	var scrollAnim = new YAHOO.util.Anim(emptyHaikuDiv, {'margin-top': { from: -90, to: 0 }}, 0.3, YAHOO.util.Easing.easeOut);
+	    	scrollAnim.onComplete.subscribe(fadeIn);
+	    	scrollAnim.animate();
 		}
 	}
-		
-	var emptyHaikuDiv = createDomFromText("<div class='haiku' id='empty_haiku'>&nbsp;</div>");
-	if (YAHOO.util.Dom.getChildren(haikuList).length === 0) {
-		haikuList.appendChild(emptyHaikuDiv);
-		emptyHaikuDiv = getEl("empty_haiku");
-		fadeIn();
-    } else {	   
-	    YAHOO.util.Dom.setStyle(emptyHaikuDiv, 'margin-top', '-90px');	
-	    emptyHaikuDiv = YAHOO.util.Dom.insertBefore(emptyHaikuDiv, YAHOO.util.Dom.getFirstChild(haikuList));
-        var scrollAnim = new YAHOO.util.Anim(emptyHaikuDiv, {'margin-top': { from: -90, to: 0 }}, 0.3, YAHOO.util.Easing.easeOut);
-	    scrollAnim.onComplete.subscribe(fadeIn);
-	    scrollAnim.animate();
-    }
 }
--- ruby/public/javascripts/button_up.js
+++ ruby/public/javascripts/button_up.js
@@ -1,4 +1,3 @@
-Village = {};
 Village.Buttons = {    
     buttonRegistry: {},
     
