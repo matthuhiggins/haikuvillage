534466d96a4482ffba66ddabdd9d2878e4003163
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2007-09-11 06:37:33 +0000

    favorites flyover
    git-svn-id: svn://spiz.us/haiku/trunk@296 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/public/javascripts/button_up.js
+++ ruby/public/javascripts/button_up.js
@@ -15,7 +15,23 @@ Village.Buttons = {
             Village.Buttons.buttonRegistry[originalId] =
                 new YAHOO.widget.Button(inputButtons[i].id, {disabled: isDisabled});
         }
-    }
+    },
+	
+	haikuFlyOver: function() {
+		function handleMouseOverOut(e, haikuDiv) {
+			var stuff = YAHOO.util.Dom.getElementsByClassName('add_to_favorites');
+			for(var i = 0; i < stuff.length; i++) {
+				YAHOO.util.Dom.setStyle(stuff[i], 'visibility', e.type === 'mouseover' ? 'visible' : 'hidden');
+			}
+		}
+		
+		var haikuDivs = YAHOO.util.Dom.getElementsByClassName('haiku');
+		for(var i = 0; i < haikuDivs.length; i++) {
+			YAHOO.util.Event.addListener(haikuDivs[i], "mouseover", handleMouseOverOut, haikuDivs[i]);
+			YAHOO.util.Event.addListener(haikuDivs[i], "mouseout", handleMouseOverOut, haikuDivs[i]);
+		}
+	}
 };
 
 YAHOO.util.Event.addListener(window, "load", Village.Buttons.makeYui);
+YAHOO.util.Event.addListener(window, "load", Village.Buttons.haikuFlyOver);
