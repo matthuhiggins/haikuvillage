139ab84b54e505aedce871b99a21f9641edf35a7
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2007-09-16 00:46:26 +0000

    Removed unused poopy
    git-svn-id: svn://spiz.us/haiku/trunk@302 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/models/haiku_comment.rb
+++
@@ -1,4 +0,0 @@
-class HaikuComment < ActiveRecord::Base
-  belongs_to :haiku
-  belongs_to :user
-end
--- ruby/app/models/haiku_tag.rb
+++
@@ -1,4 +0,0 @@
-class HaikuTag < ActiveRecord::Base
-  belongs_to :haiku
-  belongs_to :tag, :counter_cache => true
-end
