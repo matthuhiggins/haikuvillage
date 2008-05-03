912cb5ea99efcf4b6ac8c63a50f0d7e80b3a4466
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-03 22:32:07 +0000

    Upgrading linguistics.
    git-svn-id: svn://spiz.us/haiku/trunk@357 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/config/environment.rb
+++ ruby/config/environment.rb
@@ -9,24 +9,11 @@ RAILS_GEM_VERSION = '2.0'
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  # Settings in config/environments/* take precedence those specified here
-
-  # Add additional load paths for your own custom dirs
-  config.load_paths += %W( #{RAILS_ROOT}/vendor/ferret/lib  )
   config.load_paths += %W( #{RAILS_ROOT}/vendor/linguistics/lib )
   config.load_paths += %W( #{RAILS_ROOT}/vendor/lingua/en )
 
-  # Force all environments to use the same logger level 
   # (by default production uses :info, the others :debug)
   # config.log_level = :debug
 
-  config.action_controller.session = {
-    :session_key => '_rails_session',
-    :secret      => '29bcd7b28512987fd1190aa8a4a1eab9'
-  }
-
-  config.action_controller.session_store = :active_record_store
-
-  # Make Active Record use UTC-base instead of local time
   config.active_record.default_timezone = :utc
 end
