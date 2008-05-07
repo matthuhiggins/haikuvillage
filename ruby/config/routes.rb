ddcfc09a4e7d1e4706d632a91810e1b56ef5a8ec
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-07 02:59:18 +0000

    Whatever.
    git-svn-id: svn://spiz.us/haiku/trunk@374 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/config/routes.rb
+++ ruby/config/routes.rb
@@ -1,13 +1,13 @@
 ActionController::Routing::Routes.draw do |map|  
   # home page
   map.connect '', :controller => "welcome", :action => "index"
+  map.resource :session
   
   # the sick syllable counter
   map.syllables "syllables/:word.:format", :word => %r{.+}, :controller => "syllables", :action => "count"
 
   map.login 'login/', :controller => "login", :action => "index"
   map.logout 'logout/', :controller => "login", :action => "logout"
-  map.register 'register/', :controller => "login", :action => "register"
     
   # Install the default route as the lowest priority.
   map.connect ':controller/:action/:id'
