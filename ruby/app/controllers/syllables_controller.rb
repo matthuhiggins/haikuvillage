03fa858f18ee7f6c77a22fe2f05f5a7a0cecad55
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-08 04:52:09 +0000

    git-svn-id: svn://spiz.us/haiku/trunk@393 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/syllables_controller.rb
+++ ruby/app/controllers/syllables_controller.rb
@@ -1,10 +1,14 @@
 class SyllablesController < ApplicationController
   def count
-    words = params[:word].split("-").collect { |word| Word.new(URI.unescape(word)) }
+    words = params[:word].split("-").collect do |word|
+      get_cache("word_count:#{word}") { Word.new(URI.unescape(word)) }
+    end
     
     respond_to do |format|
       format.json { render :text => words.to_json }
     end
   end
+  
+  acts_as_cached
 
 end
