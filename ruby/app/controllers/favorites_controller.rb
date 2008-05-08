e22fcdbfe297f15526ea289e277098fc0fc8519b
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-08 06:06:15 +0000

    Add ajax support to favorites
    git-svn-id: svn://spiz.us/haiku/trunk@402 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/favorites_controller.rb
+++ ruby/app/controllers/favorites_controller.rb
@@ -1,12 +1,29 @@
 class FavoritesController < ApplicationController
   def update
-    current_user.favorites << Haiku.find(params[:haiku_id])
-    flash[:notice] = 'New favorite added!'
-    redirect_to haiku_url(Haiku.find(params[:haiku_id]))
+    change_favorite do |haiku|
+      current_user.favorites << haiku
+      flash[:notice] = 'New favorite added!'
+    end
   end
   
   def destroy
-    HaikuFavorite.destroy_all(:user_id => current_user, :haiku_id => params[:haiku_id])
-    render :text => "destroyed"
+    change_favorite do |haiku|
+      HaikuFavorite.destroy_all(:user_id => current_user, :haiku_id => haiku)
+      flash[:notice] = 'Favorite removed!'
+    end
   end
+  
+  private
+    def change_favorite
+      haiku = Haiku.find(params[:haiku_id])
+      yield(haiku)
+        
+      respond_to do |f|
+        f.html { redirect_to haiku }
+        f.js   { head :ok }
+      end
+        
+    rescue => e
+      head :unprocessable_entity
+    end
 end
