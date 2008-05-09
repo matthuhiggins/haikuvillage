93c9dbfe87590783c5ea9d6183f015165592caa0
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-09 20:43:28 +0000

    git-svn-id: svn://spiz.us/haiku/trunk@413 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/session_controller.rb
+++ ruby/app/controllers/session_controller.rb
@@ -1,18 +1,29 @@
 class SessionController < ApplicationController  
   def create
     user = User.authenticate(params[:username], params[:password])
-    if user
-      session[:username] = user.username
-    else
-      flash[:notice] = "Invalid user/password combination"
+    session[:username] = user.username if user
+    
+    respond_to do |f|
+      f.html do
+        if session[:username]
+          redirect_to(request.env["HTTP_REFERER"] || root_url)
+        else
+          flash[:notice] = "Invalid user/password combination"
+        end
+      end
+      f.js { head (session[:username] ? :ok : :bad_request) }
     end
-    redirect_to root_url
   end
 
   def destroy
     session[:username] = nil
-    flash[:notice] = "Logged out"
-    redirect_to root_url
+
+    respond_to do |f|
+      f.html do
+        flash[:notice] = "Logged out"
+        redirect_to root_url
+      end
+    end
   end
   
   private
