63d77053e63f38278941899fcd775fbdb86ee9ef
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-11 23:26:39 +0000

    git-svn-id: svn://spiz.us/haiku/trunk@444 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/helpers/haikus_helper.rb
+++ ruby/app/helpers/haikus_helper.rb
@@ -1,13 +1,21 @@
 module HaikusHelper
   def remote_login_form(&block)
     remote_form_for(:user,
-      :before => "$('user_submit').hide()",
-      :complete => "$('user_submit').show()",
-      :success => "$('haiku_form').submit();",
-      :failure => "$('error_box').innerHTML = 'bad login'",
-      :url => login_url,
-      :method => :post,
-      :html => {:id => 'haiku_login', :style => 'display:none'},
+      :before => update_page do |page|
+        page['user_submit']['disabled'] = true
+        page.replace_html('haiku_login_message', 'Logging in...')
+      end,
+      :success    => update_page do |page|
+        page['haiku_form'].submit
+      end,
+      :failure    => update_page do |page|
+        page['user_submit']['disabled'] = false
+        page.show('user_submit')
+        page.replace_html('haiku_login_message', 'Invalid username/password')
+      end,
+      :url        => login_url,
+      :method     => :post,
+      :html       => {:id => 'haiku_login', :style => 'display:none'},
       &block)
   end
 end
