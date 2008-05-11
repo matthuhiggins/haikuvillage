d63737251204a83b58b182e4d52542b84f2d3c8f
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-11 00:37:35 +0000

    factored out stupid haiku template files.
    git-svn-id: svn://spiz.us/haiku/trunk@421 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/application.rb
+++ ruby/app/controllers/application.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
   
   private
     def list_haikus(proxy, options = {})
-      offset = ((params[:page] || 1).to_i - 1) * HAIKUS_PER_PAGE
+      offset = (current_page - 1) * HAIKUS_PER_PAGE
       options.merge!(:offset => offset, :limit => HAIKUS_PER_PAGE)
       
       @haikus = proxy.all(options)
@@ -29,5 +29,9 @@ class ApplicationController < ActionController::Base
       session[:username] ? User.find_or_create_by_username(session[:username]) : nil
     end
     
-    helper_method :current_user
+    def current_page
+      (params[:page] || 1).to_i
+    end
+    
+    helper_method :current_user, :current_page
 end
