cb24d0c927a2f73cf3a37003b1944264fe93bad9
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-11 01:47:59 +0000

    Added some fixture data
    git-svn-id: svn://spiz.us/haiku/trunk@423 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/db/migrate/003_indexes.rb
+++ ruby/db/migrate/003_indexes.rb
@@ -2,7 +2,7 @@ class Indexes < ActiveRecord::Migration
   
   def self.up
     add_index :haikus, :user_id
-    add_index :haikus, :created_at
+    add_index :haikus, [:created_at, :haiku_favorites_count]
 
     add_index :haiku_favorites, [:user_id, :haiku_id], :unique => true
     add_index :haiku_favorites, [:haiku_id, :user_id, :created_at]
