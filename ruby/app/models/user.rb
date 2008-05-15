c05b8c616e6c4f066373e79ee88510a5e75b6f17
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-15 01:49:45 +0000

    git-svn-id: svn://spiz.us/haiku/trunk@479 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/models/user.rb
+++ ruby/app/models/user.rb
@@ -6,18 +6,19 @@ class User < ActiveRecord::Base
   validates_presence_of :username
   validates_uniqueness_of :username
   validates_presence_of :password
-  
+    
   def self.authenticate(username, password)
     http = Net::HTTP.new('twitter.com')
     http.start do |http|
       request = Net::HTTP::Get.new('/account/verify_credentials.xml')
       request.basic_auth username, password
       response = http.request(request)
-      response.value
-      find_or_create_by_username(:username => username, :password => password) if response.code[0..2].to_i == 200
+
+      if response.code[0..2].to_i >= 200 && response.code[0..2].to_i < 400
+        find_or_create_by_username(:username => username, :password => password)
+      end
     end
   rescue => e
-    logger.debug(e.message)
     return false
   end
 end
