19c55b9855ce7dd14952016f10591f95524120c5
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-22 02:50:03 +0000

    mike is getting nervous
    git-svn-id: svn://spiz.us/haiku/trunk@508 14663b11-d54d-c041-aa2c-fbbaee43d5f3

---
+++ ruby/config/initializers/logged_exception_auth.rb
@@ -0,0 +1,10 @@
+LoggedExceptionsController.class_eval do  
+  # before_filter :logged_exception_login
+  # 
+  # protected
+  #   def logged_exception_login
+  #     authenticate_or_request_with_http_basic do |user_name, password| 
+  #       user_name == 'haiku' && password == '575'
+  #     end  
+  #   end
+end
