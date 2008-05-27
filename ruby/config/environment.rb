674896625f20ba270e481d164466b18cb0f3faaa
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-27 16:27:59 +0000

    shiite
    git-svn-id: svn://spiz.us/haiku/trunk@528 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/config/environment.rb
+++ ruby/config/environment.rb
@@ -8,11 +8,7 @@ Rails::Initializer.run do |config|
   %w(linguistics lingua faster_xml_simple).each do |lib|
     config.load_paths.push("#{RAILS_ROOT}/vendor/#{lib}/lib")
   end
-  
-  ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
-    :haiku => "%b %d, %Y"
-  )
-  
+    
   config.action_controller.session = {
     :session_key => "_haiku_village",
     :secret => "I bust the stupid dope moves Esteban. I got the stupid juice."
@@ -21,3 +17,7 @@ Rails::Initializer.run do |config|
   config.time_zone = "Pacific Time (US & Canada)"
   config.active_record.default_timezone = :utc
 end
+
+ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
+  :haiku => "%b %d, %Y"
+)
