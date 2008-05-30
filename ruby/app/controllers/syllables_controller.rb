2683b81a5dc9b173b6a2a246e8392e4e1bc1a807
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-30 17:51:57 +0000

    git-svn-id: svn://spiz.us/haiku/trunk@558 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/syllables_controller.rb
+++ ruby/app/controllers/syllables_controller.rb
@@ -1,4 +1,6 @@
 class SyllablesController < ApplicationController
+  caches_action :index, :cache_path => Proc.new { |controller| controller.params[:words] }
+  
   def index
     words = params[:words].split("-").collect do |word|
       word = URI.unescape(word)
