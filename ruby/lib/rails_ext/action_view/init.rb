9dd41c8df0327b4d8b672dac211cafece89abee1
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-05-30 07:45:58 +0000

    The Current Solution
    git-svn-id: svn://spiz.us/haiku/trunk@550 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/lib/rails_ext/action_view/init.rb
+++ ruby/lib/rails_ext/action_view/init.rb
@@ -1,8 +1,10 @@
 $:.unshift(File.dirname(__FILE__))
 
 require 'lib/link_to_block'
+require 'lib/google_asset_tag_helper'
 
 ActionView::Base.class_eval do
   include HaikuView::Helpers::UrlBlockHelper
   include HaikuView::Helpers::PrototypeBlockHelper
+  include HaikuView::Helpers::GoogleAssetTagHelper
 end
