3a731738e60dda74a242927f197829f013998cd5
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-06-06 04:03:35 +0000

    ahh whaa?
    git-svn-id: svn://spiz.us/haiku/trunk@613 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/helpers/application_helper.rb
+++ ruby/app/helpers/application_helper.rb
@@ -6,4 +6,9 @@ module ApplicationHelper
   def javascript(*args)
     content_for(:head) { javascript_include_tag *args }
   end
+  
+  def link_to_controller(name, controller_name)
+    html_options = controller.controller_name == controller_name ? {:class => 'selected'} : {}
+    link_to(name, {:controller => controller_name}, html_options)
+  end
 end
