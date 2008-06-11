629a4c0c900e1df102784de66a11f5844436f249
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-06-11 17:07:22 +0000

    jebeuz
    git-svn-id: svn://spiz.us/haiku/trunk@656 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/public_controller.rb
+++ ruby/app/controllers/public_controller.rb
@@ -18,7 +18,8 @@ class PublicController < ApplicationController
       raise InvalidHaikuException unless @haiku.valid_syllables?
       @author = Author.new(params[:author])
     
-      if @author.save && (author.haikus << @haiku)
+      if @author.save
+        @author.haikus = [@haiku]
         session[:username] = @author.username
         flash[:new_haiku_id] = @haiku.id
         redirect_to :controller => 'journal'
