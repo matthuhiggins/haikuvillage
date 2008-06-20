d196c0287b34e3537c1ace5fc299e0277f8e728a
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-06-20 15:59:41 +0000

    oops
    git-svn-id: svn://spiz.us/haiku/trunk@704 14663b11-d54d-c041-aa2c-fbbaee43d5f3

---
+++ ruby/app/helpers/authors_helper.rb
@@ -0,0 +1,22 @@
+module AuthorsHelper
+  def author_cloud(authors)
+    font_sizes = {}
+    authors.sort { |author1, author2| author1.favorited_count_total <=> author2.favorited_count_total }.each_with_index do |author, index|
+      font_sizes[author.username] = number_to_percentage(80 + (120 * (index.to_f / authors.size)), :precision => 0)
+    end
+    
+    sorted_by_name = authors.sort { |author1, author2| author1.username <=> author2.username }
+    
+    sorted_by_name.map do |author|
+      link_to_author author, {:style => "font-size: #{font_sizes[author.username]}"}
+    end.join(' ')
+  end
+  
+  def author_list(authors)
+    authors.map { |author| link_to_author author }.join(', ')
+  end
+  
+  def link_to_author(author, html_options = {})
+    link_to author.username, {:controller => 'authors', :action => :show, :id => author.username}, html_options
+  end
+end
