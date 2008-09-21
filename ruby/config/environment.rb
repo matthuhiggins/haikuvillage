ce821ad80d11bb0db7c61e6798f4dfc5846c6ed7
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-09-21 21:42:57 +0000

    Upgrade
    git-svn-id: svn://spiz.us/haiku/trunk@746 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/config/environment.rb
+++ ruby/config/environment.rb
@@ -1,5 +1,5 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.1'
+RAILS_GEM_VERSION = '2.1.1'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
