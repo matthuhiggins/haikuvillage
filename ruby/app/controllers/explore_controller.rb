3e88cba6900f2ececfd114119402f974cf62bbc6
mike <mike@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-09-21 23:51:59 +0000

    randome haiku
    git-svn-id: svn://spiz.us/haiku/trunk@749 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/explore_controller.rb
+++ ruby/app/controllers/explore_controller.rb
@@ -3,5 +3,6 @@ class ExploreController < ApplicationController
     @new_authors = Author.brand_new.all(:limit => 12)
     @new_subjects = Subject.recent.all(:limit => 12)
     @popular_subjects = Subject.popular.all(:limit => 40)
+    @random_haiku = Haiku.all(:limit => 10, :order => 'created_at desc').rand
   end
 end
