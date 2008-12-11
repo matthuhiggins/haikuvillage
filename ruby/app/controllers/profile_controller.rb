e04eba36b2ddfc7405be097f9661fa7e312d8093
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-12-11 03:14:35 +0000

    git-svn-id: svn://spiz.us/haiku/trunk@771 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/profile_controller.rb
+++ ruby/app/controllers/profile_controller.rb
@@ -3,10 +3,21 @@ class ProfileController < ApplicationController
 
   def update_avatar
     if current_author.update_attributes(params[:author])
-      flash[:notice] = 'Profile saved'
+      flash[:notice] = 'Avatar saved'
     end
-    redirect_to :action => 'index'
+    redirect_to :action => 'avatar'
   end
 
-  
+  def password
+    if request.post?
+      @author = current_author
+      if @author.authenticate(params[:current_password])
+        if @author.update_attributes(:password => params[:password])
+          flash[:notice] = "Password successfully changed"
+        end
+      else
+        flash[:notice] = 'Wrong current password'
+      end
+    end
+  end
 end
