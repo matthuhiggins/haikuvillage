1697f8820bcd820ddc847738a84ac147cbc06c9f
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-12-13 02:04:17 +0000

    It's the clean ass shit
    git-svn-id: svn://spiz.us/haiku/trunk@787 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/config/environment.rb
+++ ruby/config/environment.rb
@@ -1,7 +1,5 @@
-# Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.2.2'
 
-# Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
@@ -9,15 +7,17 @@ Rails::Initializer.run do |config|
     config.load_paths.push("#{RAILS_ROOT}/vendor/#{lib}/lib")
   end
 
+  config.time_zone = "UTC"
+
+  config.gem "haml"
+  config.gem "libxml-ruby", :lib => "libxml"
+
   config.action_controller.session = {
     :session_key => "_haiku_village",
     :secret => "I bust the stupid dope moves Esteban. I got the stupid juice."
   }
 
-  # config.plugins = [:haml, :exception_logger, :will_paginate, :auto_complete]
-
   config.action_controller.cache_store = :mem_cache_store, "localhost"
-  config.time_zone = "UTC"
 end
 
 ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(
