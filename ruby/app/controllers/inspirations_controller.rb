a640174958d8e81d6ad6fd0e9f53892a77e9532b
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-12-23 07:25:32 +0000

    clean out unused inspiration code
    git-svn-id: svn://spiz.us/haiku/trunk@828 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/inspirations_controller.rb
+++ ruby/app/controllers/inspirations_controller.rb
@@ -1,15 +1,10 @@
 class InspirationsController < ApplicationController
-  def flickr
-    @inspirations = FlickrInspiration.all(:order => 'created_at desc', :limit => 12)
-    render :action => 'flickr_list'
-  end
-  
   def show
     @conversation = Conversation.find(params[:id])
     # This needs to be abstacted to use the 'inspiration_type' from the conversation table
     @inspiration = @conversation.inspiration
   end
-  
+
   def random
     offset = (params[:offset] || 1).to_i
     @inspiration = FlickrInspiration.first(:offset => offset, :order => "created_at desc")
