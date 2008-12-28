a730312e86bfcee6dcf363ea956b46a1b5c22e30
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2008-12-28 19:09:50 +0000

    Clean up haml
    git-svn-id: svn://spiz.us/haiku/trunk@843 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/config/initializers/haml.rb
+++ ruby/config/initializers/haml.rb
@@ -1,1 +1,9 @@
+require 'haml'
+
+# Load Haml and Sass
+Haml.init_rails(binding)
+
 Haml::Template.options[:attr_wrapper] = '"'
+
+Sass::Plugin.options[:always_update] = true
+Sass::Plugin.options[:style] = :expanded
--- ruby/config/initializers/sass.rb
+++
@@ -1,2 +0,0 @@
-Sass::Plugin.options[:always_update] = true
-Sass::Plugin.options[:style] = :expanded
