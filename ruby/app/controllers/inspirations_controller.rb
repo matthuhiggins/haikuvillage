dac3f7a14e4040583e9d19ca1726943c8564e422
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-01-15 03:14:10 +0000

    Cleaning stuff up
    git-svn-id: svn://spiz.us/haiku/trunk@870 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/inspirations_controller.rb
+++ ruby/app/controllers/inspirations_controller.rb
@@ -1,9 +1,4 @@
 class InspirationsController < ApplicationController
-  def show
-    @conversation = Conversation.find(params[:id])
-    @inspiration = @conversation.inspiration
-  end
-
   def random
     offset = (params[:offset] || 1).to_i
     @inspiration = FlickrInspiration.first(:offset => offset, :order => "created_at desc")
