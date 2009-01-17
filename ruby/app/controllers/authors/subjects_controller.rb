ebabbed2a5d138dd7634bf526e93fa2a20d8bb9f
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-01-17 18:36:12 +0000

    It dat clean shit
    git-svn-id: svn://spiz.us/haiku/trunk@893 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/authors/subjects_controller.rb
+++ ruby/app/controllers/authors/subjects_controller.rb
@@ -1,7 +1,7 @@
 class Authors::SubjectsController < ApplicationController
   def index
     @author = Author.find_by_username(params[:author_id])
-    @subjects = @author.haikus.count(:group => :subject_name, :conditions => 'subject_name is not null')
+    @subjects = @author.haikus.count(:group => :subject_name, :conditions => 'subject_name is not null', :order => 'count_all desc')
   end
 
   def show
--- ruby/app/helpers/application_helper.rb
+++ ruby/app/helpers/application_helper.rb
@@ -1,7 +1,8 @@
 module ApplicationHelper
   def link_to_controller(name, controller_name)
+    url = send("#{controller_name}_url")
     html_options = controller.controller_name == controller_name ? {:class => 'selected'} : {}
-    link_to(name, {:controller => controller_name}, html_options)
+    link_to(name, url, html_options)
   end
   
   def title(title, prefix = true)
