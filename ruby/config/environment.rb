1c3a7dce89a4ec209c640f205d1b11ccafef3b9b
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-01-23 22:37:23 +0000

    Add authors menu
    git-svn-id: svn://spiz.us/haiku/trunk@911 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/config/environment.rb
+++ ruby/config/environment.rb
@@ -9,7 +9,7 @@ Rails::Initializer.run do |config|
 
   config.time_zone = "UTC"
 
-  config.gem "haml",                  :version => "2.0.6"
+  config.gem "haml",                  :version => "2.0.7"
   config.gem "libxml-ruby",           :version => '0.9.7',  :lib => "libxml"
   config.gem 'mislav-will_paginate',  :version => '2.3.6',  :lib => 'will_paginate',  :source => 'http://gems.github.com'
   config.gem 'paperclip',             :version => '2.1.2'
