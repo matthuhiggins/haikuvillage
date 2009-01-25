bb01e8b2e50877b9adaeb61eccbbf35ada05461e
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-01-25 22:36:40 +0000

    Sub naving the author page
    git-svn-id: svn://spiz.us/haiku/trunk@913 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/authors/friends_controller.rb
+++ ruby/app/controllers/authors/friends_controller.rb
@@ -1,6 +1,7 @@
 class Authors::FriendsController < ApplicationController
   def index
-    @friends = Author.find_by_username(params[:author_id]).friends
+    @author = Author.find_by_username(params[:author_id])
+    @friends = @author.friends
   end
   
   def create
