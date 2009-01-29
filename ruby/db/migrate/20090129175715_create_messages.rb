aca2e441d6870b17b11fc379d01fb7c024e612d5
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-01-29 18:00:06 +0000

    Add friendships
    git-svn-id: svn://spiz.us/haiku/trunk@960 14663b11-d54d-c041-aa2c-fbbaee43d5f3

---
+++ ruby/db/migrate/20090129175715_create_messages.rb
@@ -0,0 +1,11 @@
+class CreateMessages < ActiveRecord::Migration
+  def self.up
+    create_table :messages do |t|
+      t.references :friendship, :null => false
+      t.text :text, :limit => 153, :null => false
+      t.timestamps
+    end
+    
+    add_foreign_key :messages, :friendship_id, :friendships
+  end
+end
--- ruby/db/schema.rb
+++ ruby/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090122012817) do
+ActiveRecord::Schema.define(:version => 20090129175715) do
 
   create_table "authors", :force => true do |t|
     t.string   "username",                                 :null => false
@@ -120,6 +120,15 @@ ActiveRecord::Schema.define(:version => 20090122012817) do
     t.text     "request"
     t.datetime "created_at"
   end
+
+  create_table "messages", :force => true do |t|
+    t.integer  "friendship_id", :null => false
+    t.text     "text",          :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "messages", ["friendship_id"], :name => "messages_friendship_id_fk"
 
   create_table "subjects", :force => true do |t|
     t.string   "name",                              :null => false
