3f4fd1877051ee83d74c26680d5298c5987d4a7b
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-02-02 06:44:11 +0000

    OMG return an array
    git-svn-id: svn://spiz.us/haiku/trunk@986 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/metal/syllable_counter.rb
+++ ruby/app/metal/syllable_counter.rb
@@ -2,7 +2,8 @@ class SyllableCounter
   def self.call(env)
     if env["REQUEST_URI"] =~ /^\/syllables\?words=(.*)/
       words = $1.split("-").map { |word| URI.unescape(word) }
-      [200, {"Content-Type" => "application/json"}, words.map { |word| {:text => word, :syllables => word.syllables}.to_json } ]
+      [200, {}, words.map { |word| {:text => word, :syllables => word.syllables} }.to_json ]
+      # [200, {"Content-Type" => "application/json"}, words.map { |word| {:text => word, :syllables => word.syllables}.to_json } ]
     else
       [404, {"Content-Type" => "text/html"}, "Not Found"]
     end
