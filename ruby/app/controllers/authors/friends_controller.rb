18b160fceb58ab01c17194bdd12a421fb16af26d
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-02-03 07:26:10 +0000

    Raise exception if author not found
    git-svn-id: svn://spiz.us/haiku/trunk@990 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/authors/friends_controller.rb
+++ ruby/app/controllers/authors/friends_controller.rb
@@ -1,17 +1,17 @@
 class Authors::FriendsController < ApplicationController
   def index
-    @author = Author.find_by_username(params[:author_id])
+    @author = Author.find_by_username!(params[:author_id])
     @friends = @author.friends.recently_updated
   end
   
   def create
-    @author = Author.find_by_username(params[:author_id])
+    @author = Author.find_by_username!(params[:author_id])
     current_author.friends << @author
     Mailer.deliver_new_friend(@author.email, current_author)
   end
   
   def destroy
-    @author = Author.find_by_username(params[:author_id])
+    @author = Author.find_by_username!(params[:author_id])
     Friendship.destroy_all(:author_id => current_author, :friend_id => @author)
   end
 end
--- ruby/app/controllers/authors/subjects_controller.rb
+++ ruby/app/controllers/authors/subjects_controller.rb
@@ -1,11 +1,11 @@
 class Authors::SubjectsController < ApplicationController
   def index
-    @author = Author.find_by_username(params[:author_id])
+    @author = Author.find_by_username!(params[:author_id])
     @subjects = @author.subjects
   end
 
   def show
-    @author = Author.find_by_username(params[:author_id])
+    @author = Author.find_by_username!(params[:author_id])
     @haikus = @author.haikus.recent.find_all_by_subject_name(params[:id]).paginate(
       :page      => params[:page],
       :per_page  => 10
