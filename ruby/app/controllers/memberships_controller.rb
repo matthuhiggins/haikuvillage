e994caefbdaafa87df07ba49bca78a59077ca653
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-02-15 10:10:03 +0000

    Groups crap
    git-svn-id: svn://spiz.us/haiku/trunk@1030 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/controllers/memberships_controller.rb
+++ ruby/app/controllers/memberships_controller.rb
@@ -1,13 +1,22 @@
 class MembershipsController < ApplicationController
   def index
-    
+    @authors = current_group.membershipss
   end
 
   def create
-    Membership.create
+    current_group.create_membership(:author_id => params[:id])
+  end
+  
+  def update
+    current_group
   end
   
   def destroy
-    Membership.destroy(params[:id])
+    current_group.memberships.destroy(:author_id => params[:id])
   end
+  
+  private
+    def current_group
+      @current_group ||= Group.find(param[:group_id])
+    end
 end
