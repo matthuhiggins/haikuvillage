fb515e01cf5a715ab85f2377debabf1c0586fa18
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-03-08 23:21:54 +0000

    More aptly named
    git-svn-id: svn://spiz.us/haiku/trunk@1097 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/config/initializers/css_expansion.rb
+++ ruby/config/initializers/css_expansion.rb
@@ -1,3 +1,3 @@
 ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion :haiku => [
-  'reset-min', 'common', 'layout', 'logo', 'menu', 'landing', 'register', 'haiku',
+  'reset-min', 'common', 'layout', 'icon', 'menu', 'landing', 'register', 'haiku',
   'subjects', 'journal', 'conversation', 'author', 'group']
