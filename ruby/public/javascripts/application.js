d09d4dd7d7dc9f38c7090b880c077c5b798397c2
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-03-11 04:49:51 +0000

    Redirect to firefox page if IE6
    git-svn-id: svn://spiz.us/haiku/trunk@1107 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/public/javascripts/application.js
+++ ruby/public/javascripts/application.js
@@ -1,3 +1,7 @@
+if(navigator.userAgent.indexOf("MSIE 6") >= 0) {
+  window.location = "http://www.mozilla.com/firefox/";
+}
+
 document.observe("dom:loaded", function() {
   $('haiku_search').observe('focus', function() {
     this.value = '';
