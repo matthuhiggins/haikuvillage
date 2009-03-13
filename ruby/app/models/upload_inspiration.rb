af97358040063ffb8696f1531e5a6868c91631f0
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-03-13 03:14:33 +0000

    Use the paperclip paths
    git-svn-id: svn://spiz.us/haiku/trunk@1122 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/models/upload_inspiration.rb
+++ ruby/app/models/upload_inspiration.rb
@@ -2,16 +2,12 @@ class UploadInspiration < ActiveRecord::Base
   inspired_by :upload
   has_attached_file :inspiration, :default_url => "/images/inspirations/:style.png",
                              :styles => { :large => "240x240>", :medium => "75x75>", :small => "32x32>" }
-
-  def to_path(size_type = '')
-    "/system/inspirations/#{id}/#{size_type}/#{inspiration_file_name}"
-  end
   
   def small
-    to_path("medium")
+    inspiration.url(:medium)
   end
   
   def thumbnail
-    to_path("large")
+    inspiration.url(:small)
   end
 end
