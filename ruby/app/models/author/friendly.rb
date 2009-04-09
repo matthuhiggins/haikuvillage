53ff288265555cce3f89e8ec092e375a44153b26
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-04-09 06:07:29 +0000

    F this!!
    git-svn-id: svn://spiz.us/haiku/trunk@1183 14663b11-d54d-c041-aa2c-fbbaee43d5f3

--- ruby/app/models/author/friendly.rb
+++ ruby/app/models/author/friendly.rb
@@ -1,16 +1,14 @@
 module Author::Friendly
-  class << self
-    def included(author)
-      # The authors this author is following
-      author.has_many :following, :class_name => "Friendship", :after_add => :create_mutual_friendship, :after_remove => :destroy_mutual_friendship
-      author.has_many :friends, :through => :following, :class_name => "Author", :source => :friend
-      author.has_many :mutual_friends, :through => :following, :class_name => "Author", :source => :friend, :conditions => {'friendships.mutual' => true}
-      author.has_many :mentors, :through => :following, :class_name => "Author", :source => :friend, :conditions => {'friendships.mutual' => false}
+  def self.included(author)
+    # The authors this author is following
+    author.has_many :following, :class_name => "Friendship"
+    author.has_many :friends, :through => :following, :class_name => "Author", :source => :friend
+    author.has_many :mutual_friends, :through => :following, :class_name => "Author", :source => :friend, :conditions => {'friendships.mutual' => true}
+    author.has_many :mentors, :through => :following, :class_name => "Author", :source => :friend, :conditions => {'friendships.mutual' => false}
 
-      # The authors following this author
-      author.has_many :followers, :foreign_key => :friend_id, :class_name => "Friendship"
-      author.has_many :disciples, :through => :followers, :class_name => "Author", :source => :author, :conditions => {'friendships.mutual' => false}
-    end
+    # The authors following this author
+    author.has_many :followers, :foreign_key => :friend_id, :class_name => "Friendship"
+    author.has_many :disciples, :through => :followers, :class_name => "Author", :source => :author, :conditions => {'friendships.mutual' => false}
   end
   
   def mutual?(other_author)
--- ruby/test/unit/author_test.rb
+++ ruby/test/unit/author_test.rb
@@ -1,6 +1,12 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
+  should_have_many :favorites
+  should_have_many :favorite_haikus, :through => :favorites
+  should_have_many :haikus
+  should_have_many :messages
+  should_belong_to :latest_haiku
+  
   def test_find_by_login_with_username
     assert_equal authors(:billy), Author.find_by_login(authors(:billy).username)
   end
@@ -22,5 +28,11 @@ class AuthorTest < ActiveSupport::TestCase
   def test_instance_authentication
     assert_equal authors(:billy), authors(:billy).authenticate('billy')
     assert_nil authors(:billy).authenticate('incorrect')
+  end
+  
+  def test_username_downcased
+    author = Author.new(:username => 'FOO')
+    author.valid?
+    assert_equal 'foo', author.username
   end
 end
