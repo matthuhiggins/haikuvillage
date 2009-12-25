1a44b432040663d28803a0528e2a9abbaa776938
matt <matt@14663b11-d54d-c041-aa2c-fbbaee43d5f3>
2009-12-25 19:23:06 +0000

    lolz
    git-svn-id: svn://spiz.us/haiku/trunk@1237 14663b11-d54d-c041-aa2c-fbbaee43d5f3

---
+++ ruby/app/controllers/syllables_controller.rb
@@ -0,0 +1,6 @@
+class SyllablesController < ApplicationController
+  def index
+    words = params[:words].split("-").map { |word| URI.unescape(word) }
+    render :json => words.map { |word| {:text => word, :syllables => word.syllables} }
+  end
+end
