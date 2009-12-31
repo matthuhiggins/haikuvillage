875c599cd9f8cb94b9ca0cf1fd9f645645620ae3
Matthew Higgins <matthuhiggins@roflcopter.local>
2009-12-30 22:17:57 -0800

    lulz

--- app/models/upload_inspiration.rb
+++ app/models/upload_inspiration.rb
@@ -1,13 +1,11 @@
 class UploadInspiration < ActiveRecord::Base
   inspired_by :upload
-  has_attached_file :inspiration, :default_url => "/images/inspirations/:style.png",
-                             :styles => { :large => "240x240>", :medium => "75x75>", :small => "32x32>" }
-  
+
   def small
-    inspiration.url(:medium)
+    # inspiration.url(:medium)
   end
   
   def thumbnail
-    inspiration.url(:large)
+    # inspiration.url(:large)
   end
 end
