ac5010b4e96fef63668a5288db3df0fa201e9767
Matthew Higgins <matthuhiggins@roflcopter.local>
2009-12-30 22:23:06 -0800

    ez

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   include Concerns::Session
   include HoptoadNotifier::Catcher
 
+  filter_parameter_logging :password, :password_confirmation
   layout proc { |controller| controller.request.xhr? ? nil : 'haikus' }
   exempt_from_layout 'builder'
   
