95dd7c7736dbdad8df2f5bfa2d36c59600fce904
Matthew <developer@matthewhiggins.com>
2010-03-04 12:38:35 -0800

    No need for googtac

--- vendor/plugins/googtaculous/tasks/googtaculous_tasks.rake
+++
@@ -1,4 +0,0 @@
-# desc "Explaining what the task does"
-# task :googtaculous do
-#   # Task goes here
-# end
--- vendor/plugins/googtaculous/test/helper.rb
+++
@@ -1,5 +0,0 @@
-require 'test/unit'
-require 'rubygems'
-require 'action_controller'
-require 'action_view/test_case'
-require File.dirname(__FILE__) + '/../lib/googtaculous'
