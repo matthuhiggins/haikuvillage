76070165ba5af82aa32eaf37ab8b6c875c68bea7
Matthew <developer@matthewhiggins.com>
2010-03-25 15:01:04 -0700

    oops

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ Rails::Initializer.run do |config|
   config.time_zone = 'UTC'
 
   config.gem 'haml',                    :version => '2.2.21'
-  config.gem 'will_paginate',           :version => '2.3.11'
+  config.gem 'will_paginate',           :version => '2.3.12'
   config.gem 'json',                    :version => '1.2.2'
   config.gem 'gravtastic',              :version => '2.1.3'
   config.gem 'matthuhiggins-foreigner', :version => '0.4.1',    :lib => 'foreigner'
