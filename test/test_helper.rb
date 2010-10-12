2753480f508737e525d8d6638f2dbc1ef6547a08
Matthew Higgins <developer@matthewhiggins.com>
2010-10-11 20:22:42 -0700

    assert_emails

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,9 @@
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
+module ActiveSupport
+  TestCase.class_eval do
+    include ActionMailer::TestHelper
+  end
+end
