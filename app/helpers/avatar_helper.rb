36c8c53c2786793043c62d3bf3abe08f5cb2ac46
Matthew Higgins <developer@matthewhiggins.com>
2010-10-12 19:30:18 -0700

    avatar progress

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,29 +1,34 @@
 module AvatarHelper
-  def small_avatar_url(author)
-    if fb.connected?
-      facebook_image(fb.uid, size: '16x16')
+  def small_avatar_image(author)
+    if author.fb_uid
+      facebook_image author.fb_uid, size: '16x16'
     else
-      author.gravatar_url size: 16, default: "http://www.haikuvillage.com/images/default_avatars/small.png"
+      gravatar_image author, size: 16, default: 'http://www.haikuvillage.com/images/default_avatars/small.png'
     end
   end
   
-  def medium_avatar_url(author)
-    if fb.connected?
-      facebook_image(fb.uid, size: '32x32')
+  def medium_avatar_image(author)
+    if author.fb_uid
+      facebook_image author.fb_uid, size: '32x32'
     else
-      author.gravatar_url size: 32, default: "http://www.haikuvillage.com/images/default_avatars/medium.png"
+      gravatar_image author, size: 32, default: 'http://www.haikuvillage.com/images/default_avatars/medium.png'
     end
   end
   
-  def large_avatar_url(author)
-    if fb.connected?
-      facebook_image(fb.uid, size: '64x64')
+  def large_avatar_image(author)
+    if author.fb_uid
+      facebook_image author.fb_uid, size: '64x64'
     else
-      author.gravatar_url size: 64, default: "http://www.haikuvillage.com/images/default_avatars/large.png"
+      gravatar_image author, size: 64, default: 'http://www.haikuvillage.com/images/default_avatars/large.png'
     end
   end
 
   def facebook_image(fb_uid, options)
-    image_tag "http://graph.facebook.com/#{fb_uid}/picture"#, options
+    image_tag("http://graph.facebook.com/#{fb_uid}/picture", options)
   end
+
+  def gravatar_image(author, options)
+    image_tag(author.gravatar_url options)
+  end
+  
 end
