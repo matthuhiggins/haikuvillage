b55331604c554661979fa795453aad0d68c75b0e
Michael Spiz <mike@air.local>
2010-10-12 21:00:56 -0700

    Fix

--- config/application.rb
+++ config/application.rb
@@ -23,3 +23,14 @@ module HaikuVillage
     config.filter_parameters += [:password, :password_confirmation]
   end
 end
+
+module ERB::Util
+  def html_escape(s)
+    s = s.to_s.force_encoding("utf-8")
+    if s.html_safe?
+      s
+    else
+      s.gsub(/[&"><]/) { |special| HTML_ESCAPE[special] }.html_safe
+    end
+  end
+end
