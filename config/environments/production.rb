00c87ca8b71287d5c495119dced8d0dc829273e2
Matthew Higgins <developer@matthewhiggins.com>
2010-10-12 21:00:58 -0700

    another one

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,8 +16,8 @@ HaikuVillage::Application.configure do
   }
 
   config.facebook = {
-    app_id: '90926621564',
-    secret: '6045be51632c0658b3021c5653295ca1',
+    app_id: '110364292360364',
+    secret: '120742aa5354718c03b7f73209df36ed',
     user_class_name: 'Author'
   }
 end
