dcbe501cb500b287efec32b3774e8797e118c3f8
Matthew Higgins <developer@matthewhiggins.com>
2010-11-30 16:56:10 -0800

    emails on hku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,17 +2,8 @@ HaikuVillage::Application.configure do
   config.cache_classes = true
   config.consider_all_requests_local = false
 
-  config.action_controller.perform_caching             = true
-
+  config.action_controller.perform_caching   = true
   config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.smtp_settings = {
-    :address        => "mail.spiz.us",
-    :port           => 587,
-    :domain         => "haikuvillage.com",
-    :user_name      => "village",
-    :password       => "haiku575",
-    :authentication => :login
-  }
 
   config.facebook = {
     app_id: '110364292360364',
