9c4fb0f2f3bf8fb7ba14353194a1f79b95be5c22
Matthew Higgins <developer@matthewhiggins.com>
2012-04-03 20:15:31 -0700

    fix password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,26 +1,30 @@
 class PasswordResetsController < ApplicationController
+  def index
+    @password_reset = PasswordReset.new
+  end
+
   def create
-    #   PasswordReset.create(:login => params[:login])
-    #   flash[:notice] = "You will receive an email with instructions."
-    #   redirect_to(login_path)
-    # rescue ActiveRecord::RecordNotFound
-    #   flash[:notice] = "We could not find an author with #{params[:login]}."
-    # 
-    @password_reset = PasswordReset.create(params[:password_reset])
+    PasswordReset.create(login: params[:login])
+    flash[:notice] = "You will receive an email with instructions."
+    redirect_to(login_path)
+  rescue ActiveRecord::RecordNotFound
+    flash[:notice] = "We could not find an author with #{params[:login]}."
+    redirect_to password_resets_path
   end
 
   def show
-    # @password_reset = 
+    @password_reset = PasswordReset.find_by_token!(params[:id])
   end
-  
-  def reset_password
-    @password_reset = PasswordReset.find_by_token!(params[:token])
-    if request.post?
-      session[:author_id] = @password_reset.author.id
+
+  def update
+    @password_reset = PasswordReset.find_by_token!(params[:id])
+
+    if @password_reset.update_attributes(params[:password_reset])
       @password_reset.destroy
-      @password_reset.author.update_attributes(:password => params[:password])
       flash[:notice] = "Your new password has been saved"
-      redirect_to(journal_path)
+      login_and_redirect(@password_reset.author)
+    else
+      render 'show'
     end
-  end
+  end  
 end
