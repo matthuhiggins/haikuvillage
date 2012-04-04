251b5a44f189285b0353cd5d086993dcada7d602
Matthew Higgins <developer@matthewhiggins.com>
2012-04-03 17:16:25 -0700

    lolllll

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ HaikuVillage::Application.routes.draw do
       get :friends
     end
 
-    resources :subjects, :controller => 'authors/subjects'
+    resources :subjects, controller: 'authors/subjects'
   end
-  match 'signup' => 'authors#new', :as => 'signup'
+  match 'signup' => 'authors#new', as: 'signup'
 
   resources :subjects do
     collection do
@@ -14,7 +14,7 @@ HaikuVillage::Application.routes.draw do
     end
   end
 
-  resources :haikus, :path => 'haiku' do
+  resources :haikus, path: 'haiku' do
     collection do
       get :search
     end
@@ -25,26 +25,26 @@ HaikuVillage::Application.routes.draw do
   resources :friends
   resources :favorites
 
-  resources :password_resets, :path => 'forgot'
+  resources :password_resets, path: 'forgot'
 
   controller 'public' do
-    match 'about' => :about, :as => :about
-    match 'register' => :register, :as => :register
-    match 'feedback' => :feedback, :as => :feedback
+    match 'about' => :about, as: :about
+    match 'register' => :register, as: :register
+    match 'feedback' => :feedback, as: :feedback
   end
 
   resource :session
 
   controller 'sessions' do
-    match 'login' => :new, :as => 'login'
-    match 'logout' => :destroy, :as => 'logout'
+    match 'login' => :new, as: 'login'
+    match 'logout' => :destroy, as: 'logout'
   end
 
-  match   'settings(/:action)' => 'profile', :as => :profile
-  match   'journal(/:action)' => 'journal', :as => :journal
+  match   'settings(/:action)' => 'profile', as: :profile
+  match   'journal(/:action)' => 'journal', as: :journal
 
-  match 'google_gadget' => 'public#google_gadget', :defaults => { :format => 'xml' }, :as => 'google_gadget'
+  match 'google_gadget' => 'public#google_gadget', defaults: { format: 'xml' }, as: 'google_gadget'
   match 'syllables' => 'syllables#index'
 
-  root :to => 'public#index'
+  root to: 'public#index'
 end
