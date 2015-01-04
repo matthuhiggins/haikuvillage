5ba461992ce950af016041bf810cf9b7c17f0170
Matthew Higgins <developer@matthewhiggins.com>
2015-01-04 12:54:42 -0800

    Require www subdomain

--- app/controllers/application/no_subdomain.rb
+++ app/controllers/application/no_subdomain.rb
@@ -3,13 +3,13 @@ module Application
     extend ActiveSupport::Concern
 
     included do
-      before_filter :redirect_if_any_subdomain
+      before_filter :redirect_if_no_subdomain
     end
 
     private
-      def redirect_if_any_subdomain
-        if request.subdomains.any?
-          redirect_to 'http://haikuvillage.com', status: 301
+      def redirect_if_no_subdomain
+        if request.subdomains.empty?
+          redirect_to 'http://www.haikuvillage.com', status: 301
         end
       end
   end
