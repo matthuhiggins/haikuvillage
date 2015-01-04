12b2369531900c786181378716b1968c03e2f50a
Matthew Higgins <developer@matthewhiggins.com>
2015-01-04 13:05:08 -0800

    Remove no_subdomain codez

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   include Application::DeferredHaiku
   include Application::LoginFilter
-  include Application::NoSubdomain
   include Application::RecordNotFound
   include Application::Rss
   include Application::Session
